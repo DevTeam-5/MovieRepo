67280bbdfd76abf736681eb09fdea181bc28b674
gunwoo08 <rick9808@gmail.com>
2024-08-05 16:22:39 +0900

    fix: movie 조회기능

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-import utility.Utility;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+import global.Genre;
+import movies.dtos.MoviesDto;
+import movies.services.MoviesService;
+import utility.DBUtil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // MoviesService 인스턴스 생성
+        MoviesService moviesService = new MoviesService();
+
+        // Create 테스트
+        MoviesDto newMovie = new MoviesDto();
+        newMovie.setTitle("Inception");
+        newMovie.setDirector("Christopher Nolan");
+        newMovie.setGenre(Genre.ACTION);
+        newMovie.setRel_date(Date.valueOf("2010-07-16"));
+        newMovie.setPublisher("Warner Bros");
+        newMovie.setRate(8.8);
+        newMovie.setActors("Leonardo DiCaprio, Joseph Gordon-Levitt");
+        newMovie.setAttendance(5000000);
+        newMovie.setDuration(Time.valueOf("02:28:00"));
+        moviesService.createMovie(newMovie);
+
     }
 }
--- src/movies/services/MoviesService.java
+++ src/movies/services/MoviesService.java
@@ -1,4 +1,41 @@
 package movies.services;
 
+import java.sql.Date;
+import java.util.List;
+
+import movies.dtos.MoviesDto;
+import utility.Utility;
+
 public class MoviesService {
+
+    // Create
+    public void createMovie(MoviesDto movie) {
+        String query = "INSERT INTO Movies (title, director, genre, rel_date, publisher, rate, actors, attendance, duration) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        Utility.writeData(query, movie);
+    }
+
+    // Read
+    public MoviesDto getMovie(int id) {
+        String query = "SELECT * FROM Movies WHERE m_id = " + id;
+        return Utility.readData(query, new MoviesDto());
+    }
+
+    // Update
+    public void updateMovie(MoviesDto movie) {
+        String query = "UPDATE Movies SET title = ?, director = ?, genre = ?, rel_date = ?, publisher = ?, rate = ?, actors = ?, attendance = ?, duration = ? WHERE m_id = ?";
+        Utility.writeData(query, movie);
+    }
+
+    // Delete
+    public void deleteMovie(int id) {
+        String query = "DELETE FROM Movies WHERE m_id = " + id;
+        MoviesDto movie = new MoviesDto(); // 기본 객체를 생성하여 파라미터로 전달
+        Utility.writeData(query, movie);
+    }
+
+    // List all movies
+    public List<MoviesDto> listAllMovies() {
+        String query = "SELECT * FROM Movies";
+        return Utility.readListData(query, new MoviesDto());
+    }
 }
