8ec15193eae1c40d0599f6126a551674850a9854
yechan <rladPcks980@gmail.com>
2024-08-05 18:28:13 +0900

    feat:first 예매기능구현

---
+++ src/member/dtos/ReservationDto.java
@@ -0,0 +1,63 @@
+package member.dtos;
+
+public class ReservationDto {
+    private int r_id;
+    private int user_id;
+    private int m_id;
+    private int s_id;
+    private int count;
+
+    public ReservationDto() {
+    }
+
+    public int getS_id() {
+        return s_id;
+    }
+
+    public void setS_id(int s_id) {
+        this.s_id = s_id;
+    }
+
+    public int getR_id() {
+        return r_id;
+    }
+
+    public void setR_id(int r_id) {
+        this.r_id = r_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservationDto{" +
+                "r_id=" + r_id +
+                ", user_id=" + user_id +
+                ", m_id=" + m_id +
+                ", s_id=" + s_id +
+                ", count=" + count +
+                '}';
+    }
+}
