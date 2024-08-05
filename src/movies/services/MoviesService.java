18cda96d7f396141f4bc588f0f7d8597c002e923
gunwoo08 <rick9808@gmail.com>
2024-08-05 18:31:16 +0900

    feat: findBymovieTitle 추가

--- src/movies/services/MoviesService.java
+++ src/movies/services/MoviesService.java
@@ -13,11 +13,22 @@ public class MoviesService {
         return movies;
     }
 
-
     public MoviesDto findByTitle(String title) {
         // SQL 쿼리 문자열을 동적으로 생성
         String query = "SELECT m_id, title, director, genre, rel_date, publisher, rate, actors, attendance, duration FROM movie WHERE title='" + title + "'";
         MoviesDto movies = Utility.readData(query, new MoviesDto());
+        return movies;
+    }
+
+    public List<MoviesDto> findByDirector(String director) {
+        String query = "SELECT m_id, title, director, genre, rel_date, publisher, rate, actors, attendance, duration FROM movie WHERE director='" + director + "'";
+        List<MoviesDto> movies = Utility.readListData(query, new MoviesDto());
+        return movies;
+    }
+
+    public List<MoviesDto> findByGenre(String genre) {
+        String query = "SELECT m_id, title, director, genre, rel_date, publisher, rate, actors, attendance, duration FROM movie WHERE genre='" + genre + "'";
+        List<MoviesDto> movies = Utility.readListData(query, new MoviesDto());
         return movies;
     }
 }
