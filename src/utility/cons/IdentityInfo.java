fb663aa952e2b214ab295a949f41aa6f8613bb27
sunghyun <20190171@inu.ac.kr>
2024-08-06 09:14:48 +0900

    chore : IdentityInfo파일 gitignore

--- src/utility/cons/IdentityInfo.java
+++
@@ -1,9 +0,0 @@
-package utility.cons;
-
-public final class IdentityInfo {
-    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/cinema";
-    public static final String DB_ID = "root";
-    public static final String DB_PASSWORD = "1234";
-
-    private IdentityInfo() {}
-}
